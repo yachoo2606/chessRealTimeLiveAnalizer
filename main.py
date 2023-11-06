e85866264a74b6143247cf6056c485c2c06affa4
yachoo2606 <janbialy@hotmail.com>
2023-11-06 21:11:49 +0100

    simple hand recognition

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
+import mediapipe as mp
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+mpDraw = mp.solutions.drawing_utils
 
+model = load_model('model/mp_hand_gesture')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+f = open('model/gesture.names', 'r')
+classNames = f.read().split("\n")
+f.close()
 
+cap = cv2.VideoCapture(1)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+while True:
+    _, frame = cap.read()
+    x, y, c = frame.shape
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hands.process(frameRGB)
+
+    className = ''
+
+    if result.multi_hand_landmarks:
+        landmarks = []
+        for handslms in result.multi_hand_landmarks:
+            for lm in handslms.landmark:
+                lmx = int(lm.x * x)
+                lmy = int(lm.y * y)
+                landmarks.append([lmx, lmy])
+
+            mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
+            prediction = model.predict([landmarks])
+            classID = np.argmax(prediction)
+            className = classNames[classID]
+            cv2.putText(frame,
+                        className,
+                        (10, 50),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        1,
+                        (0, 0, 255),
+                        2,
+                        cv2.LINE_AA)
+
+    cv2.imshow("Output", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
